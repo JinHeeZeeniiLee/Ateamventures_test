a580d6e8748d71d15c0cf84501a05def114613e4
JinHeeZeeniiLee <zniilee@gmail.com>
2022-04-09 04:06:30 +0900

    [add] js with tsx

--- ateamventures-test/src/App.tsx
+++ ateamventures-test/src/App.tsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Dashboard from './pages/Dashboard'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Dashboard/>
     </div>
   );
 }
---
+++ ateamventures-test/src/pages/Dashboard.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'; 
+import './Dashboard.css';
+
+const Dashboard = () => {
+
+
+    return(
+        <>
+        <nav className="nav">
+                <div className="nav-title">CAPA파트너스</div>
+                <div>
+                    <i className="fa-thin fa-buildings"></i>
+                </div>
+        </nav>
+        </>
+    )
+}
+
+export default Dashboard;
